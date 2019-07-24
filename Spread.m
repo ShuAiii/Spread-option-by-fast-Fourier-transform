ae090fe919ef4ce01de5be0632b84ef609bb219e
kevin zhang <kevin_zhang1990@hotmail.com>
2019-07-24 14:44:43 -0400

    Fixed error and added a function
    fixed a plotting range error and included Paul Godfrey's complex gamma function

--- Spread.m
+++ Spread.m
@@ -26,7 +26,7 @@ classdef Spread
     end
     
     methods(Static)    
-        function [p1,p2,C,H,HH1,HH2,HHH11,HHH12,HHH22]=FFTMatrix(S1,S2,K,r,sigma1,sigma2,rho,tau)
+        function [p1,p2,C,H,HT,HH1,HH2,HHH11,HHH12,HHH22]=FFTMatrix(S1,S2,K,r,sigma1,sigma2,rho,tau)
             N=64;
             u_bar=40;
             e1=-3;
@@ -51,6 +51,7 @@ classdef Spread
             x2 = -0.5*N*eta_star2 + eta_star2*l;
             
             H=zeros(N,N);
+            HT=zeros(N,N);
             HH1=zeros(N,N);
             HH2=zeros(N,N);
             HHH11=zeros(N,N);
@@ -58,9 +59,17 @@ classdef Spread
             HHH22=zeros(N,N);
             C=zeros(N,N);
             
+            rvec = [r,r]';
+            sigma = [sigma1^2, sigma2^2]';
+        
+            
             for i=0:N-1
                 for j=0:N-1
-                    H(i+1, j+1)=(-1)^(i+j) * Spread.Phi([u1(i+1)+1i*e1,u2(j+1)+1i*e2],r,sigma1,sigma2,rho,tau) * Spread.P_hat([u1(i+1)+1i*e1,u2(j+1)+1i*e2]);
+                    u=[u1(i+1)+1i*e1,u2(j+1)+1i*e2];
+                    mu=u*(rvec-0.5*sigma);
+                    Sigma=u*[sigma1^2, rho*sigma1*sigma2; rho*sigma1*sigma2, sigma2^2]*transpose(u);
+                    H(i+1, j+1)=(-1)^(i+j) * Spread.Phi(mu,Sigma,tau) * Spread.P_hat(u);
+                    HT(i+1, j+1)=(1i*mu-0.5*Sigma-r)*H(i+1, j+1);
                     HH1(i+1, j+1)=(1i*u1(i+1)-e1)*H(i+1, j+1);
                     HH2(i+1, j+1)=(1i*u2(j+1)-e2)*H(i+1, j+1);
                     HHH11(i+1, j+1)=(1i*u1(i+1)-e1)^2*H(i+1, j+1);
@@ -81,18 +90,21 @@ classdef Spread
             value = Vmat(p1,p2);
         end
         
-        function [delta1,delta2,gamma11,gamma12,gamma22] = Greek(S1,S2,K,r,sigma1,sigma2,rho,tau)
+        function [delta1,delta2,theta,gamma11,gamma12,gamma22] = Greek(S1,S2,K,r,sigma1,sigma2,rho,tau)
             
-            [p1,p2,C,H,HH1,HH2,HHH11,HHH12,HHH22] = Spread.FFTMatrix(S1,S2,K,r,sigma1,sigma2,rho,tau);
-            
+            [p1,p2,C,H,HT,HH1,HH2,HHH11,HHH12,HHH22] = Spread.FFTMatrix(S1,S2,K,r,sigma1,sigma2,rho,tau);
+     
             delta1mat=K/S1*exp(-0.05*tau)*real(C.*ifft2(HH1));
             delta2mat=K/S2*exp(-0.05*tau)*real(C.*ifft2(HH2));
+            thetamat=K*exp(-0.05*tau)*real(C.*ifft2(HT));
             gamma11mat=-1/S1*delta1mat+K/S1^2*exp(-0.05*tau)*real(C.*ifft2(HHH11));
             gamma12mat=K/(S1*S2)*exp(-0.05*tau)*real(C.*ifft2(HHH12));
             gamma22mat=-1/S2*delta2mat+K/S2^2*exp(-0.05*tau)*real(C.*ifft2(HHH22));
             
+           
             delta1 = delta1mat(p1,p2);
             delta2 = delta2mat(p1,p2);
+            theta=thetamat(p1,p2);
             gamma11 = gamma11mat(p1,p2);
             gamma12 = gamma12mat(p1,p2);
             gamma22 = gamma22mat(p1,p2);
@@ -111,30 +123,29 @@ classdef Spread
             value = cgamma(1i*(u(1)+u(2))-1) * cgamma(-1i*u(2)) / cgamma(1i*u(1)+1);
         end
         
-        function value = Phi(u,r,sigma1,sigma2,rho,tau)
-            rvec = [r,r]';
-            sigma = [sigma1^2, sigma2^2]';
-            Sigma = [sigma1^2, rho*sigma1*sigma2; rho*sigma1*sigma2, sigma2^2];
-            value = exp(1i*u*(rvec - 0.5*sigma)*tau - u*Sigma*transpose(u)*tau*0.5);
+        function value = Phi(mu,Sigma,tau)
+            value = exp(1i*mu*tau - Sigma*tau*0.5);
         end
         
-        function Plot(K,r,sigma1,sigma2,rho,tau,N)
-            
-            x = 1:N;
+        function Plot(K,r,sigma1,sigma2,rho,tau,M)
+            N=M/25;
+            x = N:N:M;
             X = meshgrid(x,x);
-            graphdelta1=zeros(N);
-            graphdelta2=zeros(N);
-            graphgamma11=zeros(N);
-            graphgamma12=zeros(N);
-            graphgamma22=zeros(N);
+            graphdelta1=zeros(M/N);
+            graphdelta2=zeros(M/N);
+            graphtheta=zeros(M/N);
+            graphgamma11=zeros(M/N);
+            graphgamma12=zeros(M/N);
+            graphgamma22=zeros(M/N);
             
-            for j=1:N
-                for i=1:N
+            for j=1:M/N
+                for i=1:M/N
                     S1=X(i,j);
                     S2=X(j,i);
-                    [delta1,delta2,gamma11,gamma12,gamma22] = Spread.Greek(S1,S2,K,r,sigma1,sigma2,rho,tau);
+                    [delta1,delta2,theta,gamma11,gamma12,gamma22] = Spread.Greek(S1,S2,K,r,sigma1,sigma2,rho,tau);
                     graphdelta1(i,j)=delta1;
                     graphdelta2(i,j)=delta2;
+                    graphtheta(i,j)=theta;
                     graphgamma11(i,j)=gamma11;
                     graphgamma12(i,j)=gamma12;
                     graphgamma22(i,j)=gamma22;
@@ -142,32 +153,42 @@ classdef Spread
             end
             
             figure()
-            surf(1:N,1:N,graphdelta1)
+            surf(N:N:M,N:N:M,graphdelta1)
             zlim([0 1])
             xlabel('Price of Asset 1')
             ylabel('Price of Asset 2')
             zlabel('Delta 1')
             figure()
-            surf(1:N,1:N,graphdelta2)
+            surf(N:N:M,N:N:M,graphdelta2)
             zlim([-1 0])
             xlabel('Price of Asset 1')
             ylabel('Price of Asset 2')
             zlabel('Delta 2')
             figure()
-            surf(1:N,1:N,graphgamma11)
+            surf(N:N:M,N:N:M,graphtheta)
+%             zlim([0 0.1])
             xlabel('Price of Asset 1')
             ylabel('Price of Asset 2')
-            zlabel('Gamma11 2')
+            zlabel('Theta')
             figure()
-            surf(1:N,1:N,graphgamma12)
+            surf(N:N:M,N:N:M,graphgamma11)
+            zlim([0 0.1])
             xlabel('Price of Asset 1')
             ylabel('Price of Asset 2')
-            zlabel('Gamma12 2')
+            zlabel('Gamma11')
             figure()
-            surf(1:N,1:N,graphgamma22)
+            surf(N:N:M,N:N:M,graphgamma12)
+            zlim([-0.1 0])
             xlabel('Price of Asset 1')
             ylabel('Price of Asset 2')
-            zlabel('Gamma22 2')
+            zlabel('Gamma12')
+            figure()
+            surf(N:N:M,N:N:M,graphgamma22)
+            zlim([0 0.1])
+            xlabel('Price of Asset 1')
+            ylabel('Price of Asset 2')
+            zlabel('Gamma22')
+            hello
         end
     end
 end
